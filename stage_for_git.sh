13f01e0c072dd7096066a9cac328d41f69b1be6b
Felix Mendoza <felix.mendoza@PLAM0115086.local>
2020-04-15 15:21:22 +0800

    ROOTCON Easter Egg Hunt 2020 - Time Writeup

--- stage_for_git.sh
+++ stage_for_git.sh
@@ -1,12 +1,13 @@
-find ./ -name "*.adoc" 2>/dev/null | sed -e 's/\(.*\)adoc/rm \1html 2>\/dev\/null/g' > /tmp/build_jekyll.sh
+find ./ -name "*.adoc" 2>/dev/null | sed -e 's/\(.*\)adoc/rm \1html 2>\/dev\/null/g' > ./build_jekyll.sh
 
-sh /tmp/build_jekyll.sh
+sh ./build_jekyll.sh
 
 bundle exec jekyll build
 
-find ./ -name "*.adoc" 2>/dev/null | sed -e 's/.\/\(.*\).adoc/mv _site\/\1.html \1.html/g' > /tmp/build_jekyll.sh
+find ./ -name "*.adoc" 2>/dev/null | sed -e 's/.\/\(.*\).adoc/mv _site\/\1.html .\/\1.html/g' > ./build_jekyll.sh
 
-sh /tmp/build_jekyll.sh
+sh ./build_jekyll.sh
 
+rm ./build_jekyll.sh
 rm -rf _site/
 rm -rf .jekyll-cache/
